11d3e2b40e36768c1b3de1848fe6c6334c3e0ad9
wwagner4 <wwagner4@gmail.com>
2016-02-13 13:27:41 +0100

    refactoring

--- Roundabout.elm
+++ Roundabout.elm
@@ -9,15 +9,16 @@ import Color exposing (..)
 
 
 type alias State =
-  { start : Time
-  , duration : Time
-  , id : StateId }
+  { id : StateId
+  , start : Time
+  , duration : Time }
 
-createState : Time -> Time -> StateId -> State
-createState start duration id =
-  { start = start
-  , duration = duration
-  , id = id }
+
+newState : StateId -> Time -> Time -> State
+newState id start duration =
+  { id = id
+  , start = start
+  , duration = duration }
 
 
 type StateId = A | B | C
@@ -25,70 +26,68 @@ type StateId = A | B | C
 type StateOfState = Ready | Processing
 
 
-stateOfState : State -> Time -> StateOfState
-stateOfState state time =
-  let
-    relTime = time - state.start
-  in
-    if (relTime > state.duration) then Ready
-    else Processing
-
-
-updateReadySignal : State -> Time -> State
-updateReadySignal state time =
-  case state.id of
-    A -> createState time (Time.second * 2) B
-    B -> createState time (Time.second * 0.5) C
-    C -> createState time Time.second A
-
-
-
 updateState : Time -> Maybe State -> Maybe State
 updateState time maybeState =
   let
+    stateOfState : State -> Time -> StateOfState
+    stateOfState state time =
+      let
+        relTime = time - state.start
+      in
+        if (relTime > state.duration) then Ready
+        else Processing
+
+
+    updateReadySignal : State -> Time -> State
+    updateReadySignal state time =
+      case state.id of
+        A -> newState B time (Time.second * 2)
+        B -> newState C time (Time.second * 0.5)
+        C -> newState A time (Time.second * 0.2)
+
+
     state = withDefault (initial time) maybeState
-    sofs = stateOfState state time
-    nextState = case sofs of
+    nextState = case (stateOfState state time) of
       Ready -> updateReadySignal state time
       Processing -> state
   in
     Just nextState
 
+
 initial : Time -> State
-initial time = createState time Time.second A
-
-
-stateSignal : Signal (Maybe State)
-stateSignal = Signal.foldp updateState Nothing (every (Time.second / 10))
-
-
-viewState : State -> List Form
-viewState state =
-  let
-    shape = square 250
-    (txt, bgForm) = case state.id of
-      A -> (fromString "A", filled Color.red shape)
-      B -> (fromString "B", filled Color.green shape)
-      C -> (fromString "C", filled Color.yellow shape)
-    txtForm = txt
-      |> Text.height 300
-      |> monospace
-      |> centered
-      |> toForm
-  in
-    [bgForm, txtForm]
-
+initial time = newState A time Time.second
 
 
 view : (Int, Int) -> Maybe State -> Element
 view (w, h) maybeState =
   let
+    viewState : State -> List Form
+    viewState state =
+      let
+        shape = square 250
+        (txt, bgForm) = case state.id of
+          A -> (fromString "A", filled Color.red shape)
+          B -> (fromString "B", filled Color.green shape)
+          C -> (fromString "C", filled Color.yellow shape)
+        txtForm = txt
+          |> Text.height 300
+          |> monospace
+          |> centered
+          |> toForm
+      in
+        [bgForm, txtForm]
+
+
+
     elems = case maybeState of
       Nothing -> []
       Just state -> viewState state
   in
     collage w h elems
 
+
+stateSignal : Signal (Maybe State)
+stateSignal = Signal.foldp updateState Nothing (every (Time.second / 10))
 
 
 main = Signal.map2 view dimensions stateSignal
