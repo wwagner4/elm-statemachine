0a897e8799d28a56def87b555f249707a8428d3c
wwagner4 <wwagner4@gmail.com>
2016-02-15 23:21:14 +0100

    onClickMouse position working

--- Mousetest.elm
+++ Mousetest.elm
@@ -8,13 +8,15 @@ import Window exposing (..)
 
 type alias Inp =
   { mousePos : (Float, Float)
+  , onClickMousePos : Maybe (Float, Float)
   , clickCnt : Int
   , time : Time }
 
 
-inp : (Float, Float) -> Int -> Time -> Inp
-inp (x, y) clickCnt time =
+inp : (Float, Float) -> Maybe (Float, Float) -> Int -> Time -> Inp
+inp (x, y) onClickMousePos clickCnt time =
   { mousePos = (x, y)
+  , onClickMousePos = onClickMousePos
   , clickCnt = clickCnt
   , time = time }
 
@@ -22,29 +24,45 @@ inp (x, y) clickCnt time =
 inpSig : Signal Inp
 inpSig =
   let
-    mouseSig : Signal (Int, Int)
-    mouseSig = Signal.sampleOn clicks Mouse.position
+    initialInp = inp (0, 0) Nothing 0 0
 
-    clickCountSig : Signal Int
-    clickCountSig =
-      foldp (\click total -> total + 1) 0 Mouse.clicks
 
-    timeSig : Signal Time
-    timeSig = Time.every (Time.millisecond * 100)
-
-    adjustPos : (Int, Int) -> (Int, Int) -> (Float, Float)
-    adjustPos (x, y) (w, h) =
+    continousInpSig : Signal Inp
+    continousInpSig =
       let
-        xoff = (toFloat w) / 2
-        yoff = (toFloat h) / 2
-      in
-        ((toFloat x) - xoff, (toFloat y) - yoff)
+        mouseSig : Signal (Int, Int)
+        mouseSig = Signal.sampleOn clicks Mouse.position
 
-    mouseSigAdjusted : Signal (Float, Float)
-    mouseSigAdjusted = Signal.map2 adjustPos mouseSig dimensions
+        clickCountSig : Signal Int
+        clickCountSig =
+          foldp (\click total -> total + 1) 0 Mouse.clicks
+
+        timeSig : Signal Time
+        timeSig = Time.every (Time.second )
+
+        adjustPos : (Int, Int) -> (Int, Int) -> (Float, Float)
+        adjustPos (x, y) (w, h) =
+          let
+            xoff = (toFloat w) / 2
+            yoff = (toFloat h) / 2
+          in
+            ((toFloat x) - xoff, (toFloat y) - yoff)
+
+        mouseSigAdjusted : Signal (Float, Float)
+        mouseSigAdjusted = Signal.map2 adjustPos mouseSig dimensions
+
+
+      in
+        Signal.map4 inp mouseSigAdjusted (constant Nothing) clickCountSig timeSig
+
+
+    nextInp : Inp -> Inp -> Inp
+    nextInp currentInp previousInp =
+      if currentInp.clickCnt > previousInp.clickCnt then { currentInp | onClickMousePos = Just currentInp.mousePos }
+      else currentInp
 
   in
-    Signal.map3 inp mouseSigAdjusted clickCountSig timeSig
+    foldp nextInp initialInp continousInpSig
 
 
 main = Signal.map show inpSig
