1845a40363c0998d8bb1506245401fa3050180cc
wwagner4 <wwagner4@gmail.com>
2016-02-16 07:42:12 +0100

    added new ink signal

--- Statemachine.elm
+++ Statemachine.elm
@@ -81,13 +81,15 @@ type Transition = TransitionReady | TransitionProcessing
 
 type alias Inp =
   { mousePos : (Float, Float)
+  , onClickMousePos : Maybe (Float, Float)
   , clickCnt : Int
   , time : Time }
 
 
-inp : (Float, Float) -> Int -> Time -> Inp
-inp (x, y) clickCnt time =
+inp : (Float, Float) -> Maybe (Float, Float) -> Int -> Time -> Inp
+inp (x, y) onClickMousePos clickCnt time =
   { mousePos = (x, y)
+  , onClickMousePos = onClickMousePos
   , clickCnt = clickCnt
   , time = time }
 
@@ -176,36 +178,52 @@ view (w, h) maybeModel =
     collage w h forms
 
 
-inpSig : Signal Inp
-inpSig =
+inpSig : Time -> Signal Inp
+inpSig timePeriode =
   let
-    mouseSig : Signal (Int, Int)
-    mouseSig = Signal.sampleOn clicks Mouse.position
+    initialInp = inp (0, 0) Nothing 0 0
 
-    clickCountSig : Signal Int
-    clickCountSig =
-      foldp (\click total -> total + 1) 0 Mouse.clicks
 
-    timeSig : Signal Time
-    timeSig = Time.every (Time.millisecond * 20)
-
-    adjustPos : (Int, Int) -> (Int, Int) -> (Float, Float)
-    adjustPos (x, y) (w, h) =
+    continousInpSig : Signal Inp
+    continousInpSig =
       let
-        xoff = (toFloat w) / 2
-        yoff = (toFloat h) / 2
-      in
-        ((toFloat x) - xoff, (toFloat y) - yoff)
+        mouseSig : Signal (Int, Int)
+        mouseSig = Signal.sampleOn clicks Mouse.position
 
-    mouseAdjustedSig : Signal (Float, Float)
-    mouseAdjustedSig = Signal.map2 adjustPos mouseSig dimensions
+        clickCountSig : Signal Int
+        clickCountSig =
+          foldp (\click total -> total + 1) 0 Mouse.clicks
+
+        timeSig : Signal Time
+        timeSig = Time.every timePeriode
+
+        adjustPos : (Int, Int) -> (Int, Int) -> (Float, Float)
+        adjustPos (x, y) (w, h) =
+          let
+            xoff = (toFloat w) / 2
+            yoff = (toFloat h) / 2
+          in
+            ((toFloat x) - xoff, (toFloat y) - yoff)
+
+        mouseSigAdjusted : Signal (Float, Float)
+        mouseSigAdjusted = Signal.map2 adjustPos mouseSig dimensions
+
+
+      in
+        Signal.map4 inp mouseSigAdjusted (constant Nothing) clickCountSig timeSig
+
+
+    nextInp : Inp -> Inp -> Inp
+    nextInp currentInp previousInp =
+      if currentInp.clickCnt > previousInp.clickCnt then { currentInp | onClickMousePos = Just currentInp.mousePos }
+      else currentInp
 
   in
-    Signal.map3 inp mouseAdjustedSig clickCountSig timeSig
+    foldp nextInp initialInp continousInpSig
 
 
 modelSignal : Signal (Maybe Model)
-modelSignal = Signal.foldp updateModel Nothing inpSig
+modelSignal = Signal.foldp updateModel Nothing (inpSig (Time.millisecond * 10))
 
 
 main = Signal.map2 view dimensions modelSignal
